c8bcaf8e2012863c2b4334ed5763ac14f0b3efbd
Alex <alex@seldoff.me>
2025-09-17 12:25:28 +0200

    Update service worker logging during lifecycle events

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
 self.addEventListener('install', event => {
-    //console.log('Service Worker installing...');
+    console.log('Service Worker installed');
 });
 
 self.addEventListener('activate', event => {
-    //console.log('Service Worker activating...');
+    console.log('Service Worker activated');
 });
 
 self.addEventListener('fetch', event => {
-    //console.log("fetch request")
-    //event.respondWith(new Promise())
+    console.log('Service Worker intercepted:', event.request.url);
 });
