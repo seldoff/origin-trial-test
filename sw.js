a0a26583bad5b14789964231f37b9acf261aca9c
Alex <alex@seldoff.me>
2025-09-17 12:06:44 +0200

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-// Service Worker - sw.js
-const CACHE_NAME = 'demo-cache-v1';
-
-// Install event
 self.addEventListener('install', event => {
     console.log('Service Worker installing...');
-    self.skipWaiting(); // Activate immediately
 });
 
-// Activate event
 self.addEventListener('activate', event => {
     console.log('Service Worker activating...');
-    event.waitUntil(self.clients.claim()); // Take control immediately
 });
 
 // Fetch event - intercept all requests
 self.addEventListener('fetch', event => {
+    console.log("fetch request")
     event.respondWith(new Promise())
 });
